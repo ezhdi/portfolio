06f77eae1f5f40d15a2f224c456722fb669e1b52
ezhdi <38072625+ezhdi@users.noreply.github.com>
2018-06-16 22:20:24 +0400

    Add files via upload

---
+++ sentiment_analisys_demo/readme.txt
@@ -0,0 +1,2 @@
+﻿Для запуска демо выполнить команду python.exe demo.py
+В браузере открыть страницу http://localhost/sentiment-demo
---
+++ sentiment_analisys_demo/sentiment_classifier.py
@@ -0,0 +1,51 @@
+__author__ = 'xead'
+from sklearn.externals import joblib
+
+
+class SentimentClassifier(object):
+    def __init__(self):
+        self.model = joblib.load("./LinearSVCNgramUnprocessedTextSentiment.pkl")
+        self.vectorizer = joblib.load("./NgramUnprocessedVectorizer.pkl")
+        #self.clf = joblib.load("./TextSentimentClassifier.pkl")
+        self.classes_dict = {0: "negative", 1: "positive", -1: "prediction error"}
+
+    @staticmethod
+    def get_probability_words(probability):
+        if probability < 0.55:
+            return "neutral or uncertain"
+        if probability < 0.7:
+            return "probably"
+        if probability > 0.95:
+            return "certain"
+        else:
+            return ""
+
+    def predict_text(self, text):
+        try:
+            vectorized = self.vectorizer.transform([text])
+            return self.model.predict(vectorized)[0],\
+                   self.model.predict_proba(vectorized)[0].max()
+        except:
+            print "prediction error"
+            return -1, 0.8
+
+    def predict_list(self, list_of_texts):
+        try:
+            vectorized = self.vectorizer.transform(list_of_texts)
+            return self.model.predict(vectorized),\
+                   self.model.predict_proba(vectorized)
+        except:
+            print 'prediction error'
+            return None
+
+    def get_prediction_message(self, text):
+        vectorized = self.vectorizer.transform([text])
+        #prediction = self.predict_text(text)
+        class_prediction = self.model.predict(vectorized)[0]
+        #prediction_probability = self.model.predict_proba(vectorized)[0].max()
+        return self.classes_dict[class_prediction]
+
+        #return self.clf.predict(text)
+        
+        #return self.model.predict(vectorized)
+        
